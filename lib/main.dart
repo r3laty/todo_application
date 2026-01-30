ca985ae5659290eab2a1e4780816a55756043a6b
r3laty <97298353+r3laty@users.noreply.github.com>
2026-01-30 14:56:37 +0300

    add a button add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,9 +7,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false, 
-      home: HomePage());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
 
@@ -30,34 +26,53 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(118, 52, 52, 52),
-      appBar: AppBar(backgroundColor: const Color.fromARGB(118, 52, 52, 52), 
-      title: Text("TodoApp"),
-      foregroundColor: Colors.white,),
-      body: ListView.builder(
-        itemCount: tiles,
-        itemBuilder: (context, index) => 
-          ListTile(
-            leading: Checkbox(
-              value: isDone,
-              onChanged: (newValue) {
-                setState(() {
-                  isDone = !isDone;
-                });
-              },
-            ),
-            title: TextField(
-              style: TextStyle(
-                decoration: isDone
-                    ? TextDecoration.lineThrough
-                    : TextDecoration.none,
-                    color: Colors.white  
-              ),
-              decoration: InputDecoration(
-                //border: OutlineInputBorder(),
-                hintText: 'Enter text',
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(118, 52, 52, 52),
+        title: Text("TodoApp"),
+        centerTitle: true,
+        foregroundColor: Colors.white,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: tiles,
+              itemBuilder: (context, index) => ListTile(
+                leading: Checkbox(
+                  value: isDone,
+                  onChanged: (newValue) {
+                    setState(() {
+                      isDone = !isDone;
+                    });
+                  },
+                ),
+                title: TextField(
+                  style: TextStyle(
+                    decoration: isDone
+                        ? TextDecoration.lineThrough
+                        : TextDecoration.none,
+                    decorationThickness: 5.0,
+                    color: Colors.white,
+                  ),
+                  decoration: const InputDecoration(hintText: 'Enter text'),
+                ),
               ),
             ),
           ),
+          // Кнопка под списком
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  tiles++; // Например, добавляем новую плитку
+                });
+              },
+              child: 
+              Icon(Icons.add, )
+            ),
+          ),
+        ],
       ),
     );
   }
